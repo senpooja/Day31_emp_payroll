0688466868b6eb21e53f05ad4095be35296b5570
senpooja <poojasen744055@gmail.com>
2022-08-26 17:13:55 +0530

    UC4_RetriveData

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,10 +1,12 @@
 # UC 1 
 
 create database payroll_service;
+
+# UC 2
 use payroll_service;
 select database();
 
-# UC 2
+# UC 3
 create table employee_payroll  
 (
 id int not null AUTO_INCREMENT,
@@ -12,9 +14,10 @@ name varchar(30),
 salary double not null,
 primary key (id)
 );
-# UC 3
+# UC 4
 
 INSERT into employee_payroll ( name salary, start) VALUES
 ('Bill',100000, '2018-01-03')
 ('Tarisa',100000, '2019-11-13')
 ('Charlei',100000, '2020-05-21');
+
